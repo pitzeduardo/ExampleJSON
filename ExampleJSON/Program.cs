bb3a26606ff2bb1f2a6127789a32e3c195aa4a4b
Eduardo Pitz <42384045+pitzeduardo@users.noreply.github.com>
2018-08-15 18:34:40 -0300

    Commit v1.00
    Functions to Serialzie and Deserealize JSON

---
+++ ExampleJSON/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Newtonsoft.Json;
+using Models;
+
+namespace ExampleJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- Input JSON -");
+
+            var json = SerializeJSON();
+
+            Console.WriteLine(json);
+            Console.WriteLine("- Output JSON -");
+
+            var student = DeserializeJSON(json);
+
+            Console.WriteLine(string.Format("Student.Name: {0}", student.Name));
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        private static string SerializeJSON()
+        {
+            var student = MockStudentData();
+            return JsonConvert.SerializeObject(student);
+        }
+
+        private static Student DeserializeJSON(string jsonExample)
+        {
+            return JsonConvert.DeserializeObject<Student>(jsonExample);
+        }
+
+        #region Fake Data
+
+        private static Student MockStudentData()
+        {
+            return new Student()
+            {
+                Name = "Eduardo Teste",
+                Address = "Rua Teste",
+                Email = "email@gmail.com",
+                Age = 20
+            };
+        }
+
+        #endregion 
+
+    }
+}
---
+++ ExampleJSON/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
